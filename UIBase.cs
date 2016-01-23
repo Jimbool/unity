b24da39598f1fb62225c7cb8cb4a8c2a426a3fe4
JimboLu <986652618@qq.com>
2016-01-23 12:00:19 +0800

    update

--- UIBase.cs
+++ UIBase.cs
@@ -1,5 +1,19 @@
-﻿using UnityEngine;
+﻿/*******************************************************************************
+ File Name: UIBase.cs
+ Descript:
+ 
+ Version: 1.0
+ Created Time: 2016/1/23 11:57:24
+ Compiler: .NETFrameworkv4.0.30319
+ Editor: Gvim7.4
+ Author: Jimbo
+ mail: jimboo.lu@gmail.com
+ 
+ Company: 
+*******************************************************************************/
+
 using System;
+using UnityEngine;
 
 public class UIBase : MonoBehaviour {
     
